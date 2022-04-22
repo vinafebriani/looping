62ece1eccdc44cdb1bb5700a0f34059cbc5b8b5b
vinafebriani <104155742+vinafebriani@users.noreply.github.com>
2022-04-22 19:21:15 +0700

    Add files via upload

---
+++ vinafebriani.py
@@ -0,0 +1,15 @@
+import turtle
+
+
+kuya1 = turtle.Turtle()
+kuya2 = turtle.Turtle()
+
+
+for i in range(4):
+ kuya1.forward(50)
+ kuya2.forward(50)
+ kuya1.right(90)
+
+ kuya2.left(90)
+
+turtle.done()
